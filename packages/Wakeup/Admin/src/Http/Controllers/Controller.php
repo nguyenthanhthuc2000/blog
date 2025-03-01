581fd2bc021b7b376ad6bf96e2f866252967a5a6
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2025-03-02 02:08:40 +0700

    update LaravelWakeUp

--- packages/Wakeup/Admin/src/Http/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Wakeup\Admin\Http\Controllers;
-
-use Illuminate\Routing\Controller as BaseController;
-
-class Controller extends BaseController
-{
-}
--- packages/Wakeup/Admin/src/Routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Wakeup\Admin\Http\Controllers\DashboardController;
-
-Route::prefix('admin')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('admin.index');
-});
