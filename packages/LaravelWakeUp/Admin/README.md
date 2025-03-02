b4199f7f4e4593e21fe41c0651371b9a92ec69d6
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2025-03-02 11:08:21 +0700

    add config, readme

---
+++ packages/LaravelWakeUp/Admin/README.md
@@ -0,0 +1,22 @@
+# Admin package for Laravel
+
+Laravel WakeUp Admin helps build Admin management system quickly with Laravel. Provides pre-built routes, views, and publishable configurations, making customization easy.
+
+
+## Installation
+
+Install this package with composer
+
+```bash
+  composer require laravelwakeup/admin:@dev
+```
+
+Publish configuration
+
+```bash
+  php artisan vendor:publish --tag=wakeup-admin-config
+```
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
--- packages/LaravelWakeUp/Admin/src/Providers/AdminServiceProvider.php
+++ packages/LaravelWakeUp/Admin/src/Providers/AdminServiceProvider.php
@@ -8,12 +8,19 @@ class AdminServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/wakeup-admin.php',
+            'wakeup-admin'
+        );
     }
 
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../Resources/views', 'admin');
+        $this->publishes([
+            __DIR__ . '/../../config/wakeup-admin.php' => config_path('wakeup-admin.php'),
+        ], 'wakeup-admin-config');
+        
     }
 }
